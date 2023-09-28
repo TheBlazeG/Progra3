4b3aa640d818ed635fae9d63347e3dc69d3df697
LucioDevv <hardwiredpony@gmail.com>
2023-09-28 13:44:32 -0600

    Examen Progra incompleto

---
+++ Examen1/Examen1/DoubleCell.h
@@ -0,0 +1,55 @@
+//
+// Created by Alumnos on 28/09/2023.
+//
+
+#ifndef EXAMEN1_DOUBLECELL_H
+#define EXAMEN1_DOUBLECELL_H
+
+
+class DoubleCell {
+public:
+    // Default constructor
+DoubleCell(double newValue=0);
+    // Constructor with value
+    explicit DoubleCell(double value);
+
+    // Copy constructor
+    DoubleCell(const DoubleCell& other);
+
+    //Reference constructor
+DoubleCell(DoubleCell&&rhs) noexcept;
+    // Destructor
+    ~DoubleCell();
+
+    // Assignment operator by copy
+DoubleCell& operator=(const DoubleCell & other);
+    // Assignment operator by reference
+    DoubleCell& operator=(DoubleCell&& other);
+
+    // Assignment operator for double
+    DoubleCell& operator=(double value);
+
+    // Arithmetic operators
+    DoubleCell operator +(const DoubleCell& other) const;
+    DoubleCell operator +(const double other) const;
+    DoubleCell operator -(const DoubleCell& other) const;
+    DoubleCell operator -(const double other) const;
+    DoubleCell operator *(const DoubleCell& other) const;
+    DoubleCell operator *(const double other) const;
+    DoubleCell operator /(const DoubleCell& other) const;
+    DoubleCell operator /(const double other) const;
+
+    // Setters
+void setValue(double newValue);
+    // Getters
+
+    double GetValue() const;
+
+
+private:
+    double Storedvalue;
+
+};
+
+
+#endif //EXAMEN1_DOUBLECELL_H
